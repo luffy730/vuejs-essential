c8c61b0caa5056c1985de057cf29da2ee2ab3e37
luffy <380156124@qq.com>
2019-04-10 09:59:45 +0800

    add github pages

--- config/index.js
+++ config/index.js
@@ -6,10 +6,13 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
+    
     // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vuejs-essential/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
